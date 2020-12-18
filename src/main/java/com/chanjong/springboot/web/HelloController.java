04fdc8c1df03bba9bca90a9898027d9f4e737f8d
parkchanjong <7000cj@gmail.com>
2020-12-18 23:30:21 +0900

    [Dto추가] Dto 클래스를 구현한다.

---
+++ src/main/java/com/chanjong/springboot/web/HelloController.java
@@ -0,0 +1,24 @@
+package com.chanjong.springboot.web;
+
+import com.chanjong.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
