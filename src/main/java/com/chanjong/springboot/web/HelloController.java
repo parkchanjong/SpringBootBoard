468a150080d5f45b08fe2614bd5e3b5eab103594
parkchanjong <7000cj@gmail.com>
2020-12-21 22:17:00 +0900

    Posts Entity클래스 추가
    - 테이블과 링크될 클래스

--- src/main/java/com/chanjong/springboot/web/HelloController.java
+++ src/main/java/com/chanjong/springboot/web/HelloController.java
@@ -2,8 +2,6 @@ package com.chanjong.springboot.web;
 
 import com.chanjong.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
