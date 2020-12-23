4ba9e54c7b20d18f487b095637773d0275da83f1
parkchanjong <7000cj@gmail.com>
2020-12-23 22:17:07 +0900

    게시물 조회, 수정 api컨트롤러를 구현한다
    조회 url: /api/v1/posts/{id}
    수정 url: /api/v1/posts/{id}

--- src/main/java/com/chanjong/springboot/web/PostsApiController.java
+++ src/main/java/com/chanjong/springboot/web/PostsApiController.java
@@ -2,11 +2,16 @@ package com.chanjong.springboot.web;
 
 import com.chanjong.springboot.service.PostsService;
 import com.chanjong.springboot.web.dto.PostsSaveRequestDto;
+import com.chanjong.springboot.web.dto.PostsUpdateRequestDto;
+import com.chanjong.springboot.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +22,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
