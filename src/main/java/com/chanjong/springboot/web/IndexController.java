4f05c186dba4eb4f05804320086f5d48a731bfe1
parkchanjong <7000cj@gmail.com>
2020-12-26 22:55:32 +0900

    서버 템플릿 엔진에서 사용할 수 있는 객첼르 위해 Model을 사용한다

--- src/main/java/com/chanjong/springboot/web/IndexController.java
+++ src/main/java/com/chanjong/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.chanjong.springboot.web;
 
+import com.chanjong.springboot.service.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
