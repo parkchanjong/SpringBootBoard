3fb9b0d2337f4f9383592a2166673a10492f799b
parkchanjong <7000cj@gmail.com>
2020-12-26 22:54:03 +0900

    findAllDesc 서비스를 추가한다. 조회속도 개선을 위해 (readOnly = true) 옵션을 추가한다.

--- src/main/java/com/chanjong/springboot/service/PostsService.java
+++ src/main/java/com/chanjong/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.chanjong.springboot.service;
 
 import com.chanjong.springboot.domain.posts.Posts;
 import com.chanjong.springboot.domain.posts.PostsRepository;
+import com.chanjong.springboot.web.dto.PostsListResponseDto;
 import com.chanjong.springboot.web.dto.PostsSaveRequestDto;
 import com.chanjong.springboot.web.dto.PostsUpdateRequestDto;
 import com.chanjong.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
