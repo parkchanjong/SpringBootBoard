bfc3030c696f53298f0cbf259c560e8ae36c9e45
parkchanjong <7000cj@gmail.com>
2020-12-27 00:06:57 +0900

    삭제, 게시글 리스트 가져오는 api추가

--- src/main/java/com/chanjong/springboot/web/PostsApiController.java
+++ src/main/java/com/chanjong/springboot/web/PostsApiController.java
@@ -1,17 +1,21 @@
 package com.chanjong.springboot.web;
 
 import com.chanjong.springboot.service.PostsService;
+import com.chanjong.springboot.web.dto.PostsListResponseDto;
 import com.chanjong.springboot.web.dto.PostsSaveRequestDto;
 import com.chanjong.springboot.web.dto.PostsUpdateRequestDto;
 import com.chanjong.springboot.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,8 +33,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
