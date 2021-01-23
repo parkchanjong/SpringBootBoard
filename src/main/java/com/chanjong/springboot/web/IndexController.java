b5d9bda6d5b39ec10d463779c990535e9316fce9
parkchanjong <7000cj@gmail.com>
2021-01-23 21:37:35 +0900

    구글 로그인 추가

--- src/main/java/com/chanjong/springboot/web/IndexController.java
+++ src/main/java/com/chanjong/springboot/web/IndexController.java
@@ -1,23 +1,32 @@
 package com.chanjong.springboot.web;
 
+import com.chanjong.springboot.config.auth.dto.SessionUser;
 import com.chanjong.springboot.service.PostsService;
 import com.chanjong.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import lombok.RequiredArgsConstructor;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
