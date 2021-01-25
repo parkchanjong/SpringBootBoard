c1aa7ecf1aecebb6fe2121b73d9d7be763bc972a
parkchanjong <7000cj@gmail.com>
2021-01-25 19:09:37 +0900

    [세션] 어노테이션 기반으로 변경
    - 사용자가 많지 않아 세션값을 데이터베이스에 저장하여 사용한다.

--- src/main/java/com/chanjong/springboot/web/IndexController.java
+++ src/main/java/com/chanjong/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.chanjong.springboot.web;
 
+import com.chanjong.springboot.config.auth.LoginUser;
 import com.chanjong.springboot.config.auth.dto.SessionUser;
 import com.chanjong.springboot.service.PostsService;
 import com.chanjong.springboot.web.dto.PostsResponseDto;
@@ -19,14 +20,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
-
         return "index";
     }
 
